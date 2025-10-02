ab786f3fca5ed9095f06a6ac9bf5276a38f163bd
jungletek <dumb.n.base+github@gmail.com>
2025-10-01 23:07:14 -0400

    Changed file structure

--- src/connection_pool.cpp
+++
@@ -1,7 +0,0 @@
-#include "connection_pool.hpp"
-#include "thirdparty_compat.hpp"
-#include "debug_logger.hpp"
-#include "constants.hpp"
-
-// Global connection pool instance
-ConnectionPool g_connection_pool(10); // Default pool size of 10 connections
--- src/constants.cpp
+++
@@ -1,7 +0,0 @@
-#include "constants.hpp"
-
-// This file contains ONLY IDA SDK constants and should not include third-party libraries
-// to avoid macro conflicts between IDA SDK and standard C++ libraries
-
-// Constants implementation - namespace already defined in header
-// This file exists to ensure the header is properly compiled
